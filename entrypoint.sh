0aa84cdc150fb12045417a9c127607355af3432e
Andres Munoz <andru255@gmail.com>
2016-06-07 19:04:24 -0500

    fx(entrypoint): Enhancenment for remove link

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,22 @@ export DEV_UID=${DEV_UID:=$ORIG_UID}
 export DEV_GID=${DEV_GID:=$ORIG_GID}
 
 ORIG_HOME=$(echo $ORIG_PASSWD | cut -f6 -d:)
+export NODE_MODULES=$ORIG_HOME/tasks/node_modules
 
 sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$DEV_UID:$DEV_GID:/" /etc/passwd
 sed -i -e "s/$DOCKER_USER:x:$ORIG_GID:/$DOCKER_USER:x:$DEV_GID:/" /etc/group
 
-ln -s /usr/local/node_modules $ORIG_HOME/tasks/node_modules
+if [ ! -f "$NODE_MODULES" ]; then
+  echo "linking node_modules"
+  ln -s /usr/local/node_modules $NODE_MODULES
+fi
+
 su - $DOCKER_USER -c "
   cd $ORIG_HOME/tasks &&
   $*
 "
-rm -fr $ORIG_HOME/tasks/node_modules
+
+if [ -f "$NODE_MODULES" ]; then
+  echo "removing node_modules"
+  rm -fr $NODE_MODULES
+fi
